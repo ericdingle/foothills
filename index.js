a31566998d1b30e22c68a4bd89f52e2e9fce7a36
Eric Dingle <eric.dingle@gmail.com>
2021-04-28 19:56:14 -0400

    Replace volume text with a canvas.

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
+addEventListener('load', () => {
+  const start = document.getElementById('start');
+  start.addEventListener('click', main);
+});
+
 async function main() {
   const context = new AudioContext();
   const analyser = context.createAnalyser();
+  const data = new Float32Array(analyser.fftSize);
 
   const stream = await navigator.mediaDevices.getUserMedia({audio: true});
   source = context.createMediaStreamSource(stream);
   source.connect(analyser);
 
-  const elem = document.getElementById('volume');
-  const data = new Float32Array(analyser.fftSize);
-
+  const canvas = document.getElementById('tuner').getContext('2d');
   function onFrame() {
     analyser.getFloatTimeDomainData(data);
     const volume = Math.sqrt(data.reduce((x, y) => x + y*y, 0) / data.length);
-    elem.innerText = Math.round(volume * 100);
+
+    canvas.clearRect(0, 0, 150, 100);
+    canvas.fillStyle = 'rgb(255, 0 ,0)';
+    canvas.fillRect(10, 25, Math.round(volume * 100), 10);
+
     requestAnimationFrame(onFrame);
   }
   requestAnimationFrame(onFrame);
 }
 
-addEventListener('load', () => {
-  const start = document.getElementById('start');
-  start.addEventListener('click', main);
-});
