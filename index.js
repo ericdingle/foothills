07c1eb22414f4f99937757e4dd93b7ec51500321
Eric Dingle <eric.dingle@gmail.com>
2021-05-04 14:00:15 -0400

    Add frequency volume detection.

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
+const TIME = 1, FREQ = 2;
+
 addEventListener('load', () => {
-  const start = document.getElementById('start');
-  start.addEventListener('click', main);
+  document.getElementById('time-volume').addEventListener('click', () => { main(TIME); });
+  document.getElementById('freq-volume').addEventListener('click', () => { main(FREQ); });
 });
 
-async function main() {
+async function main(type) {
   const context = new AudioContext();
   const analyser = context.createAnalyser();
-  const data = new Float32Array(analyser.fftSize);
+
+  let data;
+  if (type == TIME) {
+    data = new Float32Array(analyser.fftSize);
+  } else if (type == FREQ) {
+    data = new Float32Array(analyser.frequencyBinCount);
+  }
 
   const stream = await navigator.mediaDevices.getUserMedia({audio: true});
   source = context.createMediaStreamSource(stream);
   source.connect(analyser);
 
   const canvas = document.getElementById('tuner').getContext('2d');
+  let a = 100, b = -100;
   function onFrame() {
-    analyser.getFloatTimeDomainData(data);
-    const volume = Math.sqrt(data.reduce((x, y) => x + y*y, 0) / data.length);
+    let volume;
+    if (type == TIME) {
+      analyser.getFloatTimeDomainData(data);
+      volume = Math.sqrt(data.reduce((x, y) => x + y*y, 0) / data.length);
+    } else if (type == FREQ) {
+      analyser.getFloatFrequencyData(data);
+      volume = Math.pow(10, 0.02 * data.reduce((x, y) => Math.max(x, y)));
+    }
 
     canvas.clearRect(0, 0, 150, 100);
     canvas.resetTransform();
